17d4fda46cb80312faeebac466fa684e55a6bb52
srangamp <rangampally.shreya@gmail.com>
2017-04-04 16:51:26 -0500

    looping the questions in quiz

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -9,8 +9,27 @@
 		vm.dataService = DataService;
 		vm.questionAnswered = questionAnswered;
 		vm.activeQuestion = 0;
+		var numQuestionsAnswered=0;
+		vm.setActiveQuestion = setActiveQuestion;
+		function setActiveQuestion(){
+			var breakOut = false;
+			var quizlength = DataService.quizQuestions.length - 1;
+			while(!breakOut){
+				vm.activeQuestion = vm.activeQuestion < quizlength?++vm.activeQuestion:0;
+				 if(DataService.quizQuestions[vm.activeQuestion].selected === null){
+        		breakOut = true;
+    }
+			}
+		}
 		function questionAnswered(){
+			var quizLength = DataService.quizQuestions.length;
+			if(DataService.quizQuestions[vm.activeQuestion].selected!==null){
+				numQuestionsAnswered++;
+				if (numQuestionsAnswered >= quizLength) {
 
+				}
+			}
+			vm.setActiveQuestion();
 		}
 	}
 
